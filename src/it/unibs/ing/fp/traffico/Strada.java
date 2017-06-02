9048e94bf8c3119f35032555d0216c6bab7fcaaa
luma <luma@luma-notebook>
2017-06-02 10:11:28 +0200

    aggiornamento classe Strada

--- src/it/unibs/ing/fp/traffico/Strada.java
+++ src/it/unibs/ing/fp/traffico/Strada.java
@@ -1,5 +1,11 @@
-package it.unibs.ing.fp.medagliere;
+package it.unibs.ing.fp.traffico;
 
 public class Strada {
+	
+	private Elemento [][] mappa;
+	
+	public Strada(int nCorsie) {
+		mappa = new Elemento [nCorsie][58];
+	}
 
 }
